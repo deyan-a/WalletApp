ae95d1376950d2d45ceea18a179f4be5d0727d5c
Smilyan Dichev <dahaca6022@gmail.com>
2018-01-27 18:02:55 +0200

    Home layout, button and empty popup form

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function () {
     var $addBtn = $("#btn-img");
     var $popUpDiv = $(".pop-up-div");
@@ -9,23 +8,4 @@ $(function () {
         $popUpForm.fadeToggle(300);
     });
 
-=======
-$(function() {
-    $('.tab-panel button').on('click', function () {
-        var panel = $(this).closest('.tab-panel');
-
-        var formToShow = $(this).attr('rel');
-        var showNextForm = function() {
-            $(this).removeClass('active');
-            $('#' + formToShow).slideDown(300, function () {
-                $(this).addClass('active');
-            });
-        }
-
-    panel.find('button.active').removeClass('active');
-        $(this).addClass('active');
-
-        $('.form.active').slideUp(300, showNextForm);
-    })
->>>>>>> 9acb5713c6775681e51ee0fd4e7498e3bc3c77e0
 });
