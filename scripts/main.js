e9cf476cf6a5a5a34311f7915848e0e993e5c661
Smilyan Dichev <dahaca6022@gmail.com>
2018-01-27 18:10:49 +0200

    Revert "Home layout, button and empty popup form"
    This reverts commit ae95d1376950d2d45ceea18a179f4be5d0727d5c.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(function () {
     var $addBtn = $("#btn-img");
     var $popUpDiv = $(".pop-up-div");
@@ -8,4 +9,23 @@ $(function () {
         $popUpForm.fadeToggle(300);
     });
 
+=======
+$(function() {
+    $('.tab-panel button').on('click', function () {
+        var panel = $(this).closest('.tab-panel');
+
+        var formToShow = $(this).attr('rel');
+        var showNextForm = function() {
+            $(this).removeClass('active');
+            $('#' + formToShow).slideDown(300, function () {
+                $(this).addClass('active');
+            });
+        }
+
+    panel.find('button.active').removeClass('active');
+        $(this).addClass('active');
+
+        $('.form.active').slideUp(300, showNextForm);
+    })
+>>>>>>> 9acb5713c6775681e51ee0fd4e7498e3bc3c77e0
 });
