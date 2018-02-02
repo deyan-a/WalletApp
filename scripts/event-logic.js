a7e2d5be5341e02294c4c53bdbb37aca6ed460df
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-02-02 12:19:10 +0200

    added divs for income and expenses tab lists, added styles and functions for both lists to work with input data

--- scripts/event-logic.js
+++ scripts/event-logic.js
@@ -34,9 +34,36 @@ $(function () {
         $newList.prependTo(parent);
     }
 
+    var updateExpensesList = function (parent, date, category, amount, note) {
+
+        var $newDate = $("<div>").text(date).addClass("col-xs-3");
+        var $newCategory = $("<div>").text(category).addClass("col-xs-3");
+        var $newAmount = $("<div>").text(amount).addClass("col-xs-3");
+
+        var $newNote = $("<div>").text(note).addClass("col-xs-3");
+        var $newList = $("<li>");
+        $newList.addClass("row");
+        $newList.append($newDate, $newCategory, $newAmount, $newNote);
+        $newList.prependTo(parent);
+    }
+
+    var updateIncomeList = function (parent, date, category, amount, note) {
+        var $newDate = $("<div>").text(date).addClass("col-xs-3");
+        var $newCategory = $("<div>").text(category).addClass("col-xs-3");
+        var $newAmount = $("<div>").text(amount).addClass("col-xs-3");
+
+        var $newNote = $("<div>").text(note).addClass("col-xs-3");
+        var $newList = $("<li>");
+        $newList.addClass("row");
+        $newList.append($newDate, $newCategory, $newAmount, $newNote);
+        $newList.prependTo(parent);
+    }
+
     var $incomeTab = $("#income-tab");
     var $expenseTab = $("#expense-tab");
-    var $eventsList = $("#events-list")
+    var $eventsList = $("#events-list");
+    var $expensesEventsList = $('#expenses-events-list');
+    var $incomeEventsList = $('#income-events-list');
 
     var spendBtn = $("#income-btn-add");
     spendBtn.on("click", function () {
@@ -55,6 +82,7 @@ $(function () {
         homeChartFunc();
         incomeChartFunc();
         expensesFuncChart();
+        updateIncomeList($incomeEventsList, $incomeDate, $incomeSelect, $incomeAmount, $incomeNote);
     });
 
     var spendBtn = $("#expense-btn-add");
@@ -73,5 +101,6 @@ $(function () {
         updateTotals();
         homeChartFunc();
         expensesFuncChart();
+        updateExpensesList($expensesEventsList, $expenseDate, $expenseSelect, $expenseAmount, $expenseNote);
     });
 });
